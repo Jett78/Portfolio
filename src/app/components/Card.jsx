77c12c2c4c96c0bdf27c1676accb7b132d7e2d25
Jeet <jetdeula@gmail.com>
2024-04-10 20:39:10 +0545

    updated

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from "react";
+import { FaCode } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa";
 
-const Card = () => {
+const Card = ({ project }) => {
   return (
     <main>
-        <section className='grid justify-center items-center border rounded-xl w-fit p-3'>
-            <div>
-                <img src="./avatar.png" alt="card" className='max-h-80' />
-            </div>
+      <section className="grid justify-center items-center border rounded-xl w-fit p-3">
+        <div className="relative">
+          <img src="./avatar.png" alt="card" className="max-h-80 " />
+          <div className=" flex  gap-5 absolute top-[8rem] left-[5rem] text-6xl opacity-0 hover:opacity-100 ease-in-out duration-300">
+            <FaCode className="cursor-pointer border-2 rounded-full p-1" />
+            <FaEye className="cursor-pointer border-2 rounded-full p-1" />
+          </div>
+        </div>
 
-            <div>
-               <h2 className='text-xl font-semibold my-2'>Project One</h2>
-               <div className='flex flex-wrap  justify-around'>
-                <button className='border p-2 rounded-full hover:bg-red-600 hover:border-none'>View Code</button>
-                <button className='border p-2 rounded-full hover:bg-purple-500 hover:border-none transition'>Live Demo</button>
-               </div>
-            </div>
-          </section>
+        <div>
+          <h2 className="text-xl font-semibold my-2">{project.name}</h2>
+          <p className="w-80 text-gray-400 text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            incidunt delectus optio quod, aperiam voluptatem blanditiis,
+            asperiores accusantium amet ratione sed quo itaque tenetur corrupti
+            facere. Iusto esse doloribus nam?
+          </p>
+        </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import Card from "./Card";
 
+const projectdata = [
+    {
+        name:"Library Management System",  
+    },
+    {
+        name:"Ecommerce-App",  
+    },
+    {
+        name:"Recipe Search Hub",  
+    },
+]
 const Projects = () => {
   return (
     <main>
@@ -8,9 +19,9 @@ const Projects = () => {
         <h2 className="text-3xl font-semibold text-gray-300">My Projects</h2>
         <p className="text-gray-400 mb-10">List of completed projects</p>
         <div className="flex flex-wrap justify-center gap-20">
-            <Card/>
-            <Card/>
-            <Card/>
+               {projectdata.map((project,index) => (
+                <Card key={index} project={project}/>
+               ))}
         </div>
       </div>
     </main>
