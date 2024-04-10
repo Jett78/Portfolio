a6ef583c7d214bb82106bdd219155ae49e8a6fcc
Jeet <jetdeula@gmail.com>
2024-04-10 20:56:50 +0545

    changed

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -5,23 +5,17 @@ import { FaEye } from "react-icons/fa";
 const Card = ({ project }) => {
   return (
     <main>
-      <section className="grid justify-center items-center border rounded-xl w-fit p-3">
-        <div className="relative">
-          <img src="./avatar.png" alt="card" className="max-h-80 " />
-          <div className=" flex  gap-5 absolute top-[8rem] left-[5rem] text-6xl opacity-0 hover:opacity-100 ease-in-out duration-300">
-            <FaCode className="cursor-pointer border-2 rounded-full p-1" />
-            <FaEye className="cursor-pointer border-2 rounded-full p-1" />
-          </div>
+      <section className=" bg-neutral-900 grid justify-center text-left w-[25rem]">
+        <div>
+          <img src="./avatar.png" alt="card" className="max-h-80 min-w-[25rem] rounded-lg " />
         </div>
 
-        <div>
+        <div className="py-1 px-4">
           <h2 className="text-xl font-semibold my-2">{project.name}</h2>
-          <p className="w-80 text-gray-400 text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-            incidunt delectus optio quod, aperiam voluptatem blanditiis,
-            asperiores accusantium amet ratione sed quo itaque tenetur corrupti
-            facere. Iusto esse doloribus nam?
-          </p>
+          <div className=" flex justify-end gap-5 text-4xl">
+            <FaCode className="cursor-pointer border-2 rounded-full p-1 opacity-40 hover:opacity-100 ease-in-out duration-300" />
+            <FaEye className="cursor-pointer border-2 rounded-full p-1 opacity-40 hover:opacity-100 ease-in-out duration-300" />
+          </div>
         </div>
       </section>
     </main>
