47a61487deb6148f8e3c32f073190ff6e1926638
Jeet <jetdeula@gmail.com>
2024-04-17 17:32:08 +0545

    updated

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -7,13 +7,13 @@ const Card = ({ project }) => {
   const { name, img, gitUrl, preview } = project;
   return (
     <main>
-      <section className=" bg-neutral-900 rounded-lg grid justify-center text-left max-w-[60rem] mx-2  ">
+      <section className=" bg-neutral-900 rounded-lg grid justify-center text-left max-w-[60rem] mx-4  ">
         <div>
           <Link href={preview}><img src={img} alt="card" className="w-80 h-[15rem] rounded-lg hover:brightness-50  ease-in-out duration-300 cursor-pointer " /></Link>
         </div>
 
         <div className="py-2 px-4">
-          <h2 className="text-xl font-semibold my-2">{name}</h2>
+          <h2 className="sm:text-xl font-semibold my-2">{name}</h2>
           <div className=" flex justify-end gap-5 text-4xl">
             <Link href={gitUrl}>
               <FaCode className="cursor-pointer border-2 rounded-full p-1 opacity-40 hover:opacity-100 ease-in-out duration-300" />
