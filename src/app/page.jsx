9c7ae44477556e13ba0f6d16234fc71ab490d8d5
Jeet <jetdeula@gmail.com>
2024-06-03 19:44:05 +0545

    updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,25 @@
-"use client";
-import React, { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import About from './components/About';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
-import LocomotiveScroll from "locomotive-scroll";
+"use client"
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+// import LocomotiveScroll from "locomotive-scroll"
 
-const Page = () => {
-  useEffect(() => {
-    // Initialize LocomotiveScroll only in the client side
-    const locomotiveScroll = new LocomotiveScroll({
-      el: document.querySelector("[data-scroll-container]"),
-      smooth: true
-    });
-
-    return () => {
-      // Destroy LocomotiveScroll instance on cleanup
-      locomotiveScroll.destroy();
-    };
-  }, []);
-
+const page = () => {
+  // const locomotiveScroll = new LocomotiveScroll();   //for smooth scrolling
   return (
-    <div data-scroll-container>
-      <Navbar />
-      <HeroSection />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
+    <div>
+      <Navbar/>
+      <HeroSection/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
